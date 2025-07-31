e13c9fea7aa1c55e951261236c79abac3cdb2833
Nhat Nam <phannam28082004@gmail.com>
2025-07-31 15:13:08 +0700

    day5 update

--- day5.py
+++ day5.py
@@ -56,6 +56,28 @@ def get_phonebook():
     conn.close()
     return [{'id': row[0], 'name': row[1], 'phone': row[2], 'address': row[3]} for row in rows]
 
+@app.get("/phonebook/find",response_model=PhoneBookOut)
+def search_name(name: str):
+    conn = sqlite3.connect("phonebook.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM phonebook WHERE name = ?", (name,))
+    row = cursor.fetchone()
+    conn.close()
+    if row:
+        return {"id": row[0], "name": row[1], "phone": row[2], "address": row[3]}
+    raise HTTPException(status_code=404,detail="PhoneBook not found")
+
+@app.get("/phonebook/{pb_id}",response_model=PhoneBookOut)
+def get_pb_id(pb_id: int):
+    conn = sqlite3.connect("phonebook.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM phonebook WHERE id = ?",(pb_id,))
+    row = cursor.fetchone()
+    conn.close()
+    if row:
+        return {"id": row[0], "name": row[1], "phone": row[2], "address": row[3]}
+    raise HTTPException(status_code=404,detail="PhoneBook not found")
+       
 @app.delete("/phonebook/{pb_id}")
 def delete_id(pb_id: int):
     conn = sqlite3.connect("phonebook.db")
@@ -68,13 +90,14 @@ def delete_id(pb_id: int):
     conn.close()
     return {"message": f"PhoneBook with id {pb_id} has been deleted"}
 
-@app.get("/phonebook/search",response_model=PhoneBookOut)
-def search_name(name: str):
+@app.put("/phonebook/{pb_id}",response_model=PhoneBook)
+def update_pb(pb_id: int, pb: PhoneBook):
     conn = sqlite3.connect("phonebook.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM phonebook WHERE name = ?", (name,))
-    row = cursor.fetchone()
+    cursor.execute("UPDATE phonebook set name = ?, phone = ?, address = ? WHERE id = ?",(pb.name,pb.phone,pb.address,pb_id))
+    conn.commit()
+    if cursor.rowcount == 0:
+        conn.close()
+        raise HTTPException(status_code=404,detail="PhoneBook not found")
     conn.close()
-    if row:
-        return {"id": row[0], "name": row[1], "phone": row[2], "address": row[3]}
-    raise HTTPException(status_code=404,detail="PhoneBook not found")
+    return {**pb.dict(),"id":pb_id}
