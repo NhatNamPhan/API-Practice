732d8b947b7d3fc0e404f413b10a3fd8adf58174
Nhat Nam <phannam28082004@gmail.com>
2025-08-09 18:44:49 +0700

    day8 update

--- day8.py
+++ day8.py
@@ -27,8 +27,9 @@ def get_contacts():
     try:
         conn = get_db()
         cur = conn.cursor()
-        cur.execute("SELECT * FROM contacts")
+        cur.execute("SELECT id, name, phone FROM contacts")
         rows = cur.fetchall()
+        cur.close()
         conn.close()
         return [{"id": row[0], "name": row[1], "phone": row[2]} for row in rows]
     except Exception as e:
@@ -42,7 +43,40 @@ def insert_contact(contact: Contact):
         cur.execute("INSERT INTO contacts (name, phone) VALUES (%s, %s) RETURNING id",(contact.name,contact.phone))
         contact_id = cur.fetchone()[0]
         conn.commit()
+        cur.close()
         conn.close()
         return {**contact.model_dump(),"id":contact_id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
+    
+@app.put("/contacts/{ct_id}",response_model=ContactOut)
+def update_ct(ct_id: int, contact: Contact):
+    try:
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute("UPDATE contacts set name = %s, phone = %s WHERE id = %s RETURNING id, name, phone",(contact.name,contact.phone,ct_id))
+        row = cur.fetchone()
+        conn.commit()
+        cur.close()
+        conn.close()
+        if not row:
+            raise HTTPException(status_code=404,detail="Contact not found")
+        return {"id": row[0], "name": row[1], "phone": row[2]}
+    except Exception as e:
+        raise HTTPException(status_code=500,detail=f"Database error: {str(e)}") 
+    
+@app.delete("/contacts/{ct_id}")
+def deleted_ct(ct_id: int):
+    try:
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute("DELETE FROM contacts WHERE id = %s RETURNING id", (ct_id,))
+        deleted = cur.fetchone()
+        conn.commit()
+        cur.close()
+        conn.close()
+        if not deleted:
+            raise HTTPException(status_code=404, detail="Contact not found")
+        return {"message": f"Contact with id {ct_id} has been deleted"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")     
